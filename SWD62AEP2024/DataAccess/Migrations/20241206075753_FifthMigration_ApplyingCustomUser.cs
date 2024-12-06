2545a4ffad3cb5861d4550f58c5e3c3945a3c890
svabela5 <109699629+svabela5@users.noreply.github.com>
2024-12-06 09:30:02 +0100

    Added Login and register functionality

---
+++ SWD62AEP2024/DataAccess/Migrations/20241206075753_FifthMigration_ApplyingCustomUser.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class FifthMigration_ApplyingCustomUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "first_name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "last_name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "first_name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "last_name",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ SWD62AEP2024/Domain/Models/CustomUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class CustomUser:IdentityUser
+    {
+        public string first_name {  get; set; }
+        public string last_name { get; set; }
+    }
+}
